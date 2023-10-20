df6dfe7981adec27d038ca335e9202585bd551c8
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-20 11:20:05 +0100

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min, temp;
+
+	if (size < 2 || !array)
+		return;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		min = i;
+		for (j = i + 1; j < size - 1 + 1; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != (int)i) 
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+		i++;
+	}
+}
