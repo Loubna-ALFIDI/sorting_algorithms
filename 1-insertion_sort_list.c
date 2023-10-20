953898fa77f84f3dee765a412a443713c3e4fc45
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-20 10:28:32 +0100

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,9 @@
 #include "sort.h"
-
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * @list: Doubly linked list to be sorted
+ * Return: Nothing
+*/
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp, *current;
