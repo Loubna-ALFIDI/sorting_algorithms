98417dbc75ba048db15f8589f42f3178aa77659a
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-20 10:27:20 +0100

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp, *current;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	current = (*list)->next;
+	while (current)
+	{
+		tmp = current->next;
+		while (current->prev && current->n < current->prev->n)
+		{
+			current->prev->next = current->next;
+			if (current->next)
+				current->next->prev = current->prev;
+			current->next = current->prev;
+			current->prev = current->next->prev;
+			current->next->prev = current;
+			if (!current->prev)
+				*list = current;
+			else
+				current->prev->next = current;
+			print_list(*list);
+		}
+		current = tmp;
+	}
+
+}
