4912837c7072230bb465d11b79c8db1114dbbba3
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-20 11:22:21 +0100

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,11 @@
 #include "sort.h"
 
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size: size of array
+ * Return: Nothing
+*/
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
