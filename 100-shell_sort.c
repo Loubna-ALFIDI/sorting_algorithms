e4e1d20078d639d631b25d5c84e2e6b1fa872247
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-22 11:09:50 +0100

    Shell sort - Knuth Sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+void shell_sort(int *array, size_t size)
+{
+    size_t gap, i, j;
+    int temp;
+
+    if (size < 2 || !array)
+        return;
+
+    for  (gap = 0; gap < size; gap++)
+    {
+        for (i = gap; i < size; i++)
+        {
+            temp = array[i];
+            for (j = i; (j >= gap) && (array[j - gap] > temp); j -= gap)
+                array[j] = array[j - gap];
+            array[j] = temp;
+        }
+        print_array(array, size);
+    }
+}
