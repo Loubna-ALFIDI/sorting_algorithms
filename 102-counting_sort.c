b721a0d8d60e7228343fd8f238b26d9b0c10ba51
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-23 19:59:21 +0100

    Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,8 +2,44 @@
 
 void counting_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int min, max;
-    int *count;
+    int *counting, *sorted;
+	int max; 
+    size_t i;
+
+    if (size < 2 || !array)
+        return;
+
+    for (i = 1; i < size; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+    }
     
+    counting = malloc((max + 1) * sizeof(int));
+    if (!counting)
+        return;
+    
+    for (i = 0; i < size; i++)
+        counting[array[i]]++;
+    
+    for (i = 1; i <= (size_t) max; i++)
+        counting[i] += counting[i - 1];
+    
+    print_array(counting, max + 1);
+    sorted = malloc(size *sizeof(int));
+    if (!sorted)
+        return;
+    for (i = 0; i < size; i++)
+    {
+        sorted[counting[array[i]] - 1] = array[i];
+        counting[array[i]]--;
+    }
+    
+    for (i = 0; i < size; i++)
+        array[i] = sorted[i];
+    
+    
+    free(counting);
+    free(sorted);
+
 }
