bba975d3fd87e3d8cd3d409fcb8a76103f43b96d
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-24 01:36:46 +0100

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -18,38 +18,36 @@ void swap_array(int *array, int i, int j)
 }
 void quick_sort_recursive(int *array, int left, int right, size_t size)
 {
-    int i, j, y;
+	int i, j, y;
 
-    j = left - 1;
-    y = array[right];
-    for (i = left; i < right; i++)
-    {
-        if (array[i] < y)
-       {
-            j++;
-            swap_array(array, j, i);
-            if (j != i)
-                print_array(array, size);
-       }
-    }
-    if (array[right] < array[j + 1])
-    {
-        swap_array(array, j + 1, right);
-        print_array(array, size);
-    }
-
-    if (j > left)
-        quick_sort_recursive(array, left, j, size);
-    
-    if (i < right)
-        quick_sort_recursive(array, i, right, size);
-    
+	if (left < right)
+	{
+		j = left - 1;
+		y = array[right];
+		for (i = left; i < right; i++)
+		{
+			if (array[i] < y)
+			{
+				j++;
+				swap_array(array, j, i);
+				if (j != i)
+					print_array(array, size);
+			}
+		}
+		if (array[right] < array[j + 1])
+		{
+			swap_array(array, j + 1, right);
+			print_array(array, size);
+		}
+		quick_sort_recursive(array, left, j, size);
+		quick_sort_recursive(array, j + 2, right, size);
+	}
 }
 
 void quick_sort(int *array, size_t size)
 {
-    if (size < 2 || !array)
-        return;
+	if (size < 2 || !array)
+		return;
 
-    quick_sort_recursive(array, 0, size - 1, size);
+	quick_sort_recursive(array, 0, size - 1, size);
 }
